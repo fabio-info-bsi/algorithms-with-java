d39df0911a88d25cc8d66a86786cb9975f9eec5d
fabio.henrique <fabio.henrique@hotmart.com>
2025-01-30 21:02:07 -0300

    feat: impl algorithm palindrome recursive by String and char [].

--- src/main/java/br/com/fabex/algorithms/palindrome/Main.java
+++ src/main/java/br/com/fabex/algorithms/palindrome/Main.java
@@ -2,22 +2,14 @@ package br.com.fabex.algorithms.palindrome;
 
 public class Main {
 
-    private boolean checkWordPalindromeByString(String word) {
+    private boolean checkWordPalindrome(String word) {
         if (null == word || word.length() == 0) {
             return false;
         }
-        int length = word.length() - 1;
-        int middle = length / 2;
-        int start = 0;
-        while (start <= middle) {
-            if (word.charAt(start++) != word.charAt(length--)) {
-                return false;
-            }
-        }
-        return true;
+        return this.checkWordPalindrome(word.toCharArray());
     }
 
-    private boolean checkWordPalindromeByArrayChar(char[] word) {
+    private boolean checkWordPalindrome(char[] word) {
         if (null == word || word.length == 0) {
             return false;
         }
@@ -32,30 +24,54 @@ public class Main {
         return true;
     }
 
+    private boolean checkWordPalindromeRecursive(char[] word) {
+        if (null == word) {
+            return true;
+        }
+        return checkWordPalindromeRecursive(word, 0);
+    }
+
+    private boolean checkWordPalindromeRecursive(char[] word, int index) {
+        int middle = word.length / 2;
+        if (word.length <= 1 || index == middle) {
+            return true;
+        }
+        return word[index] == word[word.length - index - 1] ? checkWordPalindromeRecursive(word, ++index) : false;
+    }
+
     public static void main(String[] args) {
         Main main = new Main();
-        //Test checkWordPalindromeByString
-        System.out.println("xbba = " + main.checkWordPalindromeByString("xbba"));
-        System.out.println("abba = " + main.checkWordPalindromeByString("abba"));
-        System.out.println("ababa = " + main.checkWordPalindromeByString("ababa"));
-        System.out.println("xbx = " + main.checkWordPalindromeByString("xbx"));
-        System.out.println("xxx = " + main.checkWordPalindromeByString("xxx"));
-        System.out.println("xbxx = " + main.checkWordPalindromeByString("xbxx"));
-        System.out.println("x = " + main.checkWordPalindromeByString("x"));
-        System.out.println("(empty) = " + main.checkWordPalindromeByString(""));
-        System.out.println("(null) = " + main.checkWordPalindromeByString(null));
 
-        //Test checkWordPalindromeByArrayChar
-        System.out.println("lollipop = " + main.checkWordPalindromeByArrayChar("lollipop".toCharArray()));
-        System.out.println("xbba = " + main.checkWordPalindromeByArrayChar("xbba".toCharArray()));
-        System.out.println("abba = " + main.checkWordPalindromeByArrayChar("abba".toCharArray()));
-        System.out.println("ababa = " + main.checkWordPalindromeByArrayChar("ababa".toCharArray()));
-        System.out.println("xbx = " + main.checkWordPalindromeByArrayChar("xbx".toCharArray()));
-        System.out.println("xxx = " + main.checkWordPalindromeByArrayChar("xxx".toCharArray()));
-        System.out.println("xbxx = " + main.checkWordPalindromeByArrayChar("xbxx".toCharArray()));
-        System.out.println("x = " + main.checkWordPalindromeByArrayChar("x".toCharArray()));
-        System.out.println("(empty) = " + main.checkWordPalindromeByArrayChar("".toCharArray()));
-        System.out.println("(null) = " + main.checkWordPalindromeByString(null));
+        //Test Recursive
+        System.out.println("xbba = " + main.checkWordPalindromeRecursive("xbba".toCharArray()));
+        System.out.println("ababa = " + main.checkWordPalindromeRecursive("ababa".toCharArray()));
+        System.out.println(" = " + main.checkWordPalindromeRecursive("".toCharArray()));
+        System.out.println("a = " + main.checkWordPalindromeRecursive("a".toCharArray()));
+        System.out.println("aaAAbAAaa = " + main.checkWordPalindromeRecursive("aaAAbAAaa".toCharArray()));
+        System.out.println("aaAAbzAAaa = " + main.checkWordPalindromeRecursive("aaAAbzAAaa".toCharArray()));
+
+        //Test checkWordPalindrome By String
+        System.out.println("xbba = " + main.checkWordPalindrome("xbba"));
+        System.out.println("abba = " + main.checkWordPalindrome("abba"));
+        System.out.println("ababa = " + main.checkWordPalindrome("ababa"));
+        System.out.println("xbx = " + main.checkWordPalindrome("xbx"));
+        System.out.println("xxx = " + main.checkWordPalindrome("xxx"));
+        System.out.println("xbxx = " + main.checkWordPalindrome("xbxx"));
+        System.out.println("x = " + main.checkWordPalindrome("x"));
+        System.out.println("(empty) = " + main.checkWordPalindrome(""));
+        System.out.println("(null) = " + main.checkWordPalindrome((String) null));
+
+        //Test checkWordPalindrome
+        System.out.println("lollipop = " + main.checkWordPalindrome("lollipop".toCharArray()));
+        System.out.println("xbba = " + main.checkWordPalindrome("xbba".toCharArray()));
+        System.out.println("abba = " + main.checkWordPalindrome("abba".toCharArray()));
+        System.out.println("ababa = " + main.checkWordPalindrome("ababa".toCharArray()));
+        System.out.println("xbx = " + main.checkWordPalindrome("xbx".toCharArray()));
+        System.out.println("xxx = " + main.checkWordPalindrome("xxx".toCharArray()));
+        System.out.println("xbxx = " + main.checkWordPalindrome("xbxx".toCharArray()));
+        System.out.println("x = " + main.checkWordPalindrome("x".toCharArray()));
+        System.out.println("(empty) = " + main.checkWordPalindrome("".toCharArray()));
+        System.out.println("(null) = " + main.checkWordPalindrome((char[]) null));
 
     }
 }
