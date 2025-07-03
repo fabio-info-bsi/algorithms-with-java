8dbc8b7c5bc63f56df41f14e19e779f41458e3c6
Fabex <fabio.henrique.santos.ti@gmail.com>
2025-07-03 08:21:52 -0300

    feat: Hash function multiplication impl;

--- src/main/java/br/com/fabex/dataofstructs/hashtable/hashfunction/address/closed/impl/HashFunctionMultiplication.java
+++ src/main/java/br/com/fabex/dataofstructs/hashtable/hashfunction/address/closed/impl/HashFunctionMultiplication.java
@@ -5,13 +5,24 @@ import br.com.fabex.dataofstructs.hashtable.hashfunction.address.closed.HashFunc
 public class HashFunctionMultiplication<T> implements HashFunctionClosedAddress<T> {
 
     protected int tableSize;
+    /**
+     * Constant range: 0 < A < 1
+     */
+    protected double constantA;
 
     public HashFunctionMultiplication(int tableSize) {
+        this(tableSize, .14);
+    }
+
+    public HashFunctionMultiplication(int tableSize, double constant) {
         this.tableSize = tableSize;
+        this.constantA = constant;
     }
 
     @Override
     public int hash(T element) {
-        return 0;
+        int key = element.hashCode();
+        double factionPart = (key * constantA) - Math.floor(key * constantA);
+        return (int) (tableSize * factionPart);
     }
 }
